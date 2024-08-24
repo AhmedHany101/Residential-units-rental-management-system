d9fe5d0a99438d9a4a08f2b4c7117b42dad3250f
AhmedHany101 <88096847+AhmedHany101@users.noreply.github.com>
2024-08-24 12:30:50 +0300

    Update layout.blade.php

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -213,11 +213,7 @@
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>ICANFORSOFTWARE</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-
-      Designed by <a href="">ICANFORSOFTWARE</a>
+      &copy; Copyright <strong><span>Alamir</span></strong>. All Rights Reserved
     </div>
   </footer><!-- End Footer -->
 
